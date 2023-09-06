d6ce86643dcb010636ab93dd1fa28127f8ad43af
tracer-ysyx <tracer@ysyx.org>
2023-09-06 23:42:41 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:42:41 up  1:14,  1 user,  load average: 0.62, 0.56, 0.51

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -83,7 +83,7 @@ static int isnum(char argc){
 static long my_atoi(const char *args){
   long res = 0;
   for (int i = 0; i < 18;i++){
-    printf("%-3d: %c", i, args[i]);
+    printf("%-3d: %c\n", i, args[i]);
     if (isnum(args[i]))
     {
       res *= 10;
