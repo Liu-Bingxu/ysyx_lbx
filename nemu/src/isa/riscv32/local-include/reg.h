c47380300e27f040167e0a8a8a090069bdf54655
tracer-ysyx <tracer@ysyx.org>
2023-09-11 23:24:56 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-31-generic #31~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Aug 16 13:45:26 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:24:56 up  4:02,  1 user,  load average: 1.17, 1.18, 1.12

--- nemu/src/isa/riscv32/local-include/reg.h
+++ nemu/src/isa/riscv32/local-include/reg.h
@@ -19,7 +19,7 @@
 #include <common.h>
 
 static inline int check_reg_idx(int idx) {
-    printf("now you are %d\n", idx);
+    // printf("now you are %d\n", idx);
     IFDEF(CONFIG_RT_CHECK, assert(idx >= 0 && idx < MUXDEF(CONFIG_RVE, 16, 32)));
     return idx;
 }
