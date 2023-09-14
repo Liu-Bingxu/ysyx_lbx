72e66adf750c92df85978e24f32117a70defd4ed
tracer-ysyx <tracer@ysyx.org>
2023-09-14 21:07:55 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:07:55 up  2:12,  1 user,  load average: 2.33, 1.93, 1.71

--- nemu/scripts/native.mk
+++ nemu/scripts/native.mk
@@ -36,7 +36,7 @@ run-env: $(BINARY) $(DIFF_REF_SO)
 run: run-env
 	$(call git_commit, "run NEMU")
 	$(NEMU_EXEC)
-# $(shell echo CFLAGS=$(CFLAGS))
+	$(shell echo CFLAGS=$(CFLAGS))
 
 gdb: run-env
 	$(call git_commit, "gdb NEMU")
