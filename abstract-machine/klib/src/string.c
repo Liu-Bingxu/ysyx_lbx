0bfe8b427fa3b3919a9d115b2c4a9b1d5e36073f
tracer-ysyx <tracer@ysyx.org>
2023-09-15 20:27:28 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:27:28 up  2:14,  1 user,  load average: 1.85, 1.26, 1.19

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -133,7 +133,7 @@ void *memmove(void *dst, const void *src, size_t n) {
         if ((src<dst)&&((src+n)>dst)){
             int first = src + n - dst;
             int second = n - first;
-            memcpy((dst + second), dst, first);
+            memmove((dst + second), dst, first);
             memcpy(dst, src, second);
         }
         else{
