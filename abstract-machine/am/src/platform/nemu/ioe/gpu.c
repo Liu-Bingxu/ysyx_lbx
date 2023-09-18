47caaa4becf8848ce16dda3f0f238fcf48b37e46
tracer-ysyx <tracer@ysyx.org>
2023-09-19 02:12:36 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:12:36 up  6:48,  1 user,  load average: 0.87, 0.66, 0.51

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,8 +6,8 @@
 
 void __am_gpu_init() {
     int i;
-    int w = io_read(AM_GPU_CONFIG).width / 32;
-    int h = io_read(AM_GPU_CONFIG).height / 32;
+    int w = io_read(AM_GPU_CONFIG).width;
+    int h = io_read(AM_GPU_CONFIG).height;
     uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
     for (i = 0; i < w * h;i++)
         fb[i] = i;
