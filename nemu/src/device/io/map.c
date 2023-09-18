9adb80fd01d4c1db97e7cbdd0a7ebaf9259ab597
tracer-ysyx <tracer@ysyx.org>
2023-09-18 22:54:11 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:54:11 up  3:29,  1 user,  load average: 0.49, 0.69, 0.69

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -53,18 +53,24 @@ void init_map() {
 }
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
-  assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
-  paddr_t offset = addr - map->low;
-  invoke_callback(map->callback, offset, len, false); // prepare data to read
-  word_t ret = host_read(map->space + offset, len);
-  return ret;
+    assert(len >= 1 && len <= 8);
+    check_bound(map, addr);
+    paddr_t offset = addr - map->low;
+    invoke_callback(map->callback, offset, len, false); // prepare data to read
+    word_t ret = host_read(map->space + offset, len);
+    IFDEF(CONFIG_DTRACE, Log_func(ANSI_FMT("DTRACE READ", ANSI_FG_YELLOW) ": "
+                                                                     "call to " ANSI_FMT("%s", ANSI_FG_GREEN) " " ANSI_FMT("%d", ANSI_FG_BLACK) " Byte data with addr: " ANSI_FMT(FMT_PADDR, ANSI_FG_BLUE) "\n",
+                                  map->name, len, addr));
+    return ret;
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
-  assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
-  paddr_t offset = addr - map->low;
-  host_write(map->space + offset, len, data);
-  invoke_callback(map->callback, offset, len, true);
+    assert(len >= 1 && len <= 8);
+    check_bound(map, addr);
+    paddr_t offset = addr - map->low;
+    host_write(map->space + offset, len, data);
+    invoke_callback(map->callback, offset, len, true);
+    IFDEF(CONFIG_DTRACE, Log_func(ANSI_FMT("DTRACE WRITE", ANSI_FG_YELLOW) ": "
+                                                                     "call to " ANSI_FMT("%s", ANSI_FG_GREEN) " " ANSI_FMT("%d", ANSI_FG_BLACK) " Byte data with addr: " ANSI_FMT(FMT_PADDR, ANSI_FG_BLUE) "\n",
+                                  map->name, len, addr));
 }
