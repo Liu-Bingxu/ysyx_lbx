ece08c24f502bdd79820455564e9c8300cffee89
tracer-ysyx <tracer@ysyx.org>
2023-09-20 01:01:22 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:01:22 up  5:47,  1 user,  load average: 0.70, 0.86, 1.01

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -41,7 +41,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
     int len = ctl->buf.end - ctl->buf.start;
     void *start = ctl->buf.start;
     while (count < len){
-        printf("Hello\n");
+        printf("I am %d, target is %d\n",len,count);
         count = inl(AUDIO_COUNT_ADDR);
     }
     if(last_len<len){
