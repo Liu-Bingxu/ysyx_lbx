e4f202078227d72a6e352445383f602fbe2170be
Liu Bingxu <qwe15889844242@163.com>
2023-09-19 14:11:51 +0800

    finish the keybrd and vga of nemu

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,23 +1,54 @@
 #include <am.h>
 #include <nemu.h>
+#include "klib.h"
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
+static int width = 0;
+static int height = 0;
+static int vmemsz = 0;
+
 void __am_gpu_init() {
+    int i;
+    int w = io_read(AM_GPU_CONFIG).width;
+    int h = io_read(AM_GPU_CONFIG).height;
+    printf("width: %d height: %d\n", w, h);
+    uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+    for (i = 0; i < w * h;i++)
+        fb[i] = i;
+    outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
-  };
+    cfg->present = true;
+    cfg->has_accel = false;
+    if(width==0){
+        cfg->width = (inl(VGACTL_ADDR) >> 16);
+        cfg->height = (inl(VGACTL_ADDR) & 0xffff);
+        cfg->vmemsz = cfg->width * cfg->height;
+        width = cfg->width;
+        height = cfg->height;
+        vmemsz = cfg->vmemsz;
+    }
+    else{
+        cfg->width = width;
+        cfg->height = height;
+        cfg->vmemsz = vmemsz;
+    }
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-  if (ctl->sync) {
-    outl(SYNC_ADDR, 1);
-  }
+    // assert((ctl->x + ctl->w) < io_read(AM_GPU_CONFIG).width);
+    // assert((ctl->y + ctl->h) < io_read(AM_GPU_CONFIG).height);
+    if(ctl->pixels!=NULL){
+        for (int i = 0; i < ctl->h; i++){
+            memcpy(((void *)FB_ADDR + (ctl->y + i) * io_read(AM_GPU_CONFIG).width * 4 + ctl->x * 4), ctl->pixels, ctl->w * 4);
+            ctl->pixels += ctl->w * 4;
+        }
+    }
+    if (ctl->sync){
+        outl(SYNC_ADDR, 1);
+    }
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -4,6 +4,19 @@
 #define KEYDOWN_MASK 0x8000
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  kbd->keydown = 0;
-  kbd->keycode = AM_KEY_NONE;
+    uint32_t key = inl(KBD_ADDR);
+    if(key==0){
+        kbd->keydown = 0;
+        kbd->keycode = AM_KEY_NONE;
+    }
+    else if((key&KEYDOWN_MASK)==0){
+        kbd->keydown = 1;
+        kbd->keycode = key;
+    }
+    else{
+        kbd->keydown = 0;
+        kbd->keycode = (key & 0x7fUL);
+    }
+//         kbd->keydown = 0;
+//         kbd->keycode = AM_KEY_NONE;
 }
