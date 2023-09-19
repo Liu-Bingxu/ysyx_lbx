c9ba2bc344b54492cf5ef9403958aeaf7c6ce5b0
tracer-ysyx <tracer@ysyx.org>
2023-09-19 23:27:10 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:27:10 up  4:13,  1 user,  load average: 1.09, 1.22, 0.80

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -13,7 +13,7 @@ void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-    cfg->present = true;
+    cfg->present = false;
     cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
