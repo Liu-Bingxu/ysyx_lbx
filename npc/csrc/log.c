df22e60c3127a8aeb01e037cd07a9cc840fbfe5c
tracer-ysyx <tracer@ysyx.org>
2023-09-21 00:58:11 +0800

    > sim RTL ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:58:11 up  1:55,  1 user,  load average: 0.56, 0.51, 0.49

--- npc/csrc/log.c
+++ npc/csrc/log.c
@@ -9,7 +9,7 @@ void init_log(const char *log_file){
     // printf("%s\n", "w");
     log_fp = fopen(log_file, "w");
     assert(log_fp);
-    Log("Open log file " ANSI_FMT("%s", ANSI_FG_YELLOW) " succces\n",log_file);
+    Log("Open log file %s succces\n",log_file);
 }
 
 bool log_enable(){
