3c5070fd235645a590039ca51cd31fe0d10ef451
tracer-ysyx <tracer@ysyx.org>
2023-09-22 00:30:15 +0800

    > sim RTL ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-32-generic #32~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 18 10:40:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:30:15 up  4:36,  1 user,  load average: 1.07, 1.34, 1.53

--- npc/csrc/difftest/dut.c
+++ npc/csrc/difftest/dut.c
@@ -14,8 +14,10 @@
 ***************************************************************************************/
 
 #include <dlfcn.h>
-
+#include "debug.h"
 #include <utils.h>
+#include "pmem.h"
+#include "difftest.h"
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
@@ -23,6 +25,8 @@ void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 
 extern void irangbuf_printf();
+
+static CPU_state cpu;
 
 #ifdef CONFIG_DIFFTEST
 
@@ -84,20 +88,20 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
       "This will help you a lot for debugging, but also significantly reduce the performance. "
       "If it is not necessary, you can turn it off in menuconfig.", ref_so_file);
   ref_difftest_init(port);
-  ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
+  ref_difftest_memcpy(PC_RST, guest_to_host(PC_RST), img_size, DIFFTEST_TO_REF);
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
-static void checkregs(CPU_state *ref, vaddr_t pc) {
+static void checkregs(CPU_state *ref, paddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
       IFDEF(CONFIG_ITRACE, irangbuf_printf());
-      nemu_state.state = NEMU_ABORT;
-      nemu_state.halt_pc = pc;
+      npc_state.state = NPC_ABORT;
+      npc_state.halt_pc = pc;
       isa_reg_display();
   }
 }
 
-void difftest_step(vaddr_t pc, vaddr_t npc) {
+void difftest_step(paddr_t pc, paddr_t npc) {
   CPU_state ref_r;
 
   if (skip_dut_nr_inst > 0) {
@@ -127,7 +131,4 @@ void difftest_step(vaddr_t pc, vaddr_t npc) {
 }
 #else
 void init_difftest(char *ref_so_file, long img_size, int port) { }
-void difftest_skip_ref(){}
-void difftest_skip_dut(int nr_ref, int nr_dut){}
-void difftest_step(vaddr_t pc, vaddr_t npc) {}
 #endif
