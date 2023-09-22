e8e3f301bfa7e5065fd2cf1e3d95cc22c1966441
tracer-ysyx <tracer@ysyx.org>
2023-09-23 00:45:44 +0800

    > sim RTL ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:45:44 up  3:45,  1 user,  load average: 1.39, 0.93, 0.76

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -95,12 +95,7 @@ void sim_rst(){
     step_and_dump_wave();
     top->sys_clk = !top->sys_clk;
     step_and_dump_wave();
-    top->sys_clk = !top->sys_clk;
-    step_and_dump_wave();
-    top->sys_clk = !top->sys_clk;
-    step_and_dump_wave();
-    top->sys_clk = !top->sys_clk;
-    step_and_dump_wave();
+
 }
 
 void halt(int code,int pc){
