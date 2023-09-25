9003fa19eb89eff0847b6807b7d3ef6b422a09f3
tracer-ysyx <tracer@ysyx.org>
2023-09-25 23:31:44 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:31:44 up  4:58,  1 user,  load average: 2.30, 1.67, 1.35

--- abstract-machine/am/include/arch/riscv.h
+++ abstract-machine/am/include/arch/riscv.h
@@ -7,10 +7,20 @@
 #define NR_REGS 32
 #endif
 
+// struct Context {
+//   TODO: fix the order of these members to match trap.S
+//   uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
+//   void *pdir;
+// };
+
 struct Context {
-  // TODO: fix the order of these members to match trap.S
-  uintptr_t mepc, mcause, gpr[NR_REGS], mstatus;
-  void *pdir;
+    union {
+        void *pdir;
+        uintptr_t gpr[NR_REGS];
+    };
+    uintptr_t mcause;
+    uintptr_t mstatus;
+    uintptr_t mepc;
 };
 
 #ifdef __riscv_e
