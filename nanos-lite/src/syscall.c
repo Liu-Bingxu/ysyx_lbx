36cdd349aa5b7375fd75be390b90e3c8df89f8dc
tracer-ysyx <tracer@ysyx.org>
2023-09-26 22:30:05 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:30:05 up  2:59,  1 user,  load average: 0.58, 0.87, 0.80

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,15 +1,25 @@
 #include <common.h>
 #include "syscall.h"
+
+int SYS_exit(int code){
+    halt(code);
+}
+
 int SYS_yield(){
     yield();
     return 0;
 }
 
 void do_syscall(Context *c) {
-  uintptr_t a[4];
-  a[0] = c->GPR1;
+    uintptr_t a[4];
+    a[0] = c->GPR1;
+    a[1] = c->GPR2;
+    a[2] = c->GPR3;
+    a[3] = c->GPR4;
 
-  switch (a[0]) {
+    switch (a[0]){
+    case sys_exit:
+        SYS_exit(a[1]);
     case sys_yield:
         c->GPRx=SYS_yield();
         c->mepc += 4;
