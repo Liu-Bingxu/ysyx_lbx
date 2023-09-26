f09daec21ecbb955d48f31adaa05be70447cf7b2
tracer-ysyx <tracer@ysyx.org>
2023-09-26 22:23:56 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:23:56 up  2:53,  1 user,  load average: 0.90, 0.73, 0.70

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,10 +1,20 @@
 #include <common.h>
 #include "syscall.h"
+int SYS_yield(){
+    yield();
+    return 0;
+}
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-    default: panic("Unhandled syscall ID = %d", a[0]);
+    case sys_yield:
+        c->GPRx=SYS_yield();
+        return;
+    default:
+        panic("Unhandled syscall ID = %d", a[0]);
   }
 }
+
