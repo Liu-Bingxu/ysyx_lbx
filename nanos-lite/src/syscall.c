68ae3c86e54377bd0eade6bba03baab67e647c04
tracer-ysyx <tracer@ysyx.org>
2023-09-26 22:40:22 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:40:22 up  3:09,  1 user,  load average: 0.52, 0.58, 0.68

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,6 +10,29 @@ int SYS_yield(){
     return 0;
 }
 
+const char *sys_call_name[] = {
+    TOSTRING(sys_exit),
+    TOSTRING(sys_yield),
+    // sys_open,
+    // sys_read,
+    // sys_write,
+    // sys_kill,
+    // sys_getpid,
+    // sys_close,
+    // sys_lseek,
+    // sys_brk,
+    // sys_fstat,
+    // sys_time,
+    // sys_signal,
+    // sys_execve,
+    // sys_fork,
+    // sys_link,
+    // sys_unlink,
+    // sys_wait,
+    // sys_times,
+    // sys_gettimeofday
+};
+
 void do_syscall(Context *c) {
     uintptr_t a[4];
     a[0] = c->GPR1;
@@ -22,6 +45,7 @@ void do_syscall(Context *c) {
         SYS_exit(a[1]);
     case sys_yield:
         c->GPRx=SYS_yield();
+        Log("%s", sys_call_name[a[0]]);
         c->mepc += 4;
         return;
     default:
