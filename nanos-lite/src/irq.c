caf1d0d42337f0ae0b8a9331f80ae5077e245676
tracer-ysyx <tracer@ysyx.org>
2023-09-26 22:26:46 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:26:46 up  2:56,  1 user,  load average: 1.20, 0.97, 0.80

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -5,6 +5,7 @@ static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
         Log("this is a event of yield");
+        c->mepc += 4;
         break;
     case EVENT_SYSCALL:
         do_syscall(c);
