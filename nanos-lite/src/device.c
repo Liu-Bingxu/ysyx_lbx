170248c90c32c34369f846e71518a8495d0dd6c4
tracer-ysyx <tracer@ysyx.org>
2023-09-28 02:58:43 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:58:43 up  8:29,  1 user,  load average: 1.63, 1.70, 1.36

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -46,6 +46,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
     AM_GPU_CONFIG_T gpu_cfg = io_read(AM_GPU_CONFIG);
+    printf("%lld\n", offset);
     int x = (offset % gpu_cfg.width) / 4;
     int y = (offset / gpu_cfg.height) / 4;
     io_write(AM_GPU_FBDRAW, x, y, buf, (len) / 4, 1, true);
