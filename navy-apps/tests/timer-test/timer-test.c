021bb722f65dcbef89ad64d7baf9fc34e89e8824
tracer-ysyx <tracer@ysyx.org>
2023-09-27 22:04:16 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:04:16 up  3:34,  1 user,  load average: 0.68, 0.59, 0.49

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -13,7 +13,9 @@ int main(int argc , char* argv[]){
 
     assert(succes == 0);
 
-    printf("Now sec is %d, and usec is %d\n", tv.tv_sec, tv.tv_usec);
+    printf("Now sec is %ld, and usec is %ld\n", tv.tv_sec, tv.tv_usec);
+
+    printf("the size of timeval is %d, the size of tv_sec is %d, the size of tv_usec is %d\n", sizeof(struct timeval), sizeof(tv.tv_sec), sizeof(tv.tv_usec));
 
     exit(0);
 }
