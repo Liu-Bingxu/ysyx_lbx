d8640baefb856921a557dcf515fbae2662f80fed
tracer-ysyx <tracer@ysyx.org>
2023-09-27 22:23:58 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:23:58 up  3:54,  1 user,  load average: 1.20, 0.83, 0.62

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -4,30 +4,41 @@
 #include "stdbool.h"
 #include "assert.h"
 #include "stdint.h"
+#include "NDL.h"
 // #include "time.h"
+
 
 int main(int argc , char* argv[]){
 
-    struct timeval tv,tv_now;
+    // struct timeval tv,tv_now;
 
-    int succes = gettimeofday(&tv, NULL);
+    // int succes = gettimeofday(&tv, NULL);
 
-    assert(succes == 0);
+    // assert(succes == 0);
 
-    printf("Now sec is %ld, and usec is %ld\n", tv.tv_sec, tv.tv_usec);
+    // printf("Now sec is %ld, and usec is %ld\n", tv.tv_sec, tv.tv_usec);
 
-    printf("the size of timeval is %d, the size of tv_sec is %d, the size of tv_usec is %d\n", sizeof(struct timeval), sizeof(tv.tv_sec), sizeof(tv.tv_usec));
+    // printf("the size of timeval is %d, the size of tv_sec is %d, the size of tv_usec is %d\n", sizeof(struct timeval), sizeof(tv.tv_sec), sizeof(tv.tv_usec));
 
     uint64_t times = 1;
 
+    uint32_t rtc_old, rtc_new;
+
+    rtc_old = NDL_GetTicks();
+
     while(1){
-        assert(gettimeofday(&tv_now, NULL) == 0);
-        if(((tv_now.tv_sec*10)-(tv.tv_sec*10))<5){
+        // assert(gettimeofday(&tv_now, NULL) == 0);
+        // if(((tv_now.tv_sec*10)-(tv.tv_sec*10))<5){
+            // continue;
+        // }
+        // tv = tv_now;
+        rtc_new = NDL_GetTicks();
+        if((rtc_new-rtc_old)<500){
             continue;
         }
-        tv = tv_now;
+        rtc_old = rtc_new;
         printf("Hello world!! It is the %lld times\n", times);
-        
+
         times++;
     }
 
