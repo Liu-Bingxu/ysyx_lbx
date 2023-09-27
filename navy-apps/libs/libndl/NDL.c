c3c10a00dd8c3d65ad395a97a4fa94b37bc8d887
tracer-ysyx <tracer@ysyx.org>
2023-09-28 01:45:09 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:45:09 up  7:15,  1 user,  load average: 0.98, 1.08, 0.80

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -59,12 +59,12 @@ void NDL_OpenCanvas(int *w, int *h) {
     else{
         FILE *dispinfo = fopen("/proc/dispinfo", "r");
         assert(dispinfo);
-        char char_buf[64];
+        char char_buf[21];
         fread(char_buf, sizeof(char_buf), 1, dispinfo);
         fclose(dispinfo);
         int width, height;
         sscanf(char_buf, "WIDTH:%d\nHEIGHT:%d", &width, &height);
-        printf("%s\n", char_buf);
+        // printf("%s\n", char_buf);
         // assert((*w) <= width);
         // assert((*h) <= height);
         screen_w = (*w)?(*w):width;
