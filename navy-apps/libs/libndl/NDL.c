be2539bfa4dab1a54a4574cb17fcb22904b5a986
tracer-ysyx <tracer@ysyx.org>
2023-09-28 17:48:42 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:48:42 up  3:14,  1 user,  load average: 3.34, 2.91, 2.52

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -30,7 +30,7 @@ int NDL_PollEvent(char *buf, int len) {
     int event_fp = open("/dev/events", 0, 0);
     int read_len = read(event_fp, buf, len);
     close(event_fp);
-    printf("%d\n", read_len);
+    // printf("%d\n", read_len);
     if (read_len == 0)
         return 0;
     else
