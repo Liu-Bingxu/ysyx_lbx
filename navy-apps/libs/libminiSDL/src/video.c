18417d9ce9ac23e7e624f72d52d74e20f065d4f2
tracer-ysyx <tracer@ysyx.org>
2023-09-28 16:39:25 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:39:25 up  2:05,  1 user,  load average: 2.45, 2.35, 2.01

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -74,7 +74,7 @@ SDL_Surface* SDL_CreateRGBSurface(uint32_t flags, int width, int height, int dep
 
 SDL_Surface* SDL_CreateRGBSurfaceFrom(void *pixels, int width, int height, int depth,
     int pitch, uint32_t Rmask, uint32_t Gmask, uint32_t Bmask, uint32_t Amask) {
-    assert(0);
+    // assert(0);
     SDL_Surface *s = SDL_CreateRGBSurface(SDL_PREALLOC, width, height, depth,
                                           Rmask, Gmask, Bmask, Amask);
     assert(pitch == s->pitch);
@@ -83,7 +83,7 @@ SDL_Surface* SDL_CreateRGBSurfaceFrom(void *pixels, int width, int height, int d
 }
 
 void SDL_FreeSurface(SDL_Surface *s) {
-    assert(0);
+    // assert(0);
   if (s != NULL) {
     if (s->format != NULL) {
       if (s->format->palette != NULL) {
