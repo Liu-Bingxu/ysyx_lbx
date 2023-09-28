f7cf59acc97a2949d51956042556207628ad1b33
tracer-ysyx <tracer@ysyx.org>
2023-09-28 16:36:50 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:50 up  2:03,  1 user,  load average: 2.90, 2.26, 1.92

--- navy-apps/tests/cpp-test/main.cpp
+++ navy-apps/tests/cpp-test/main.cpp
@@ -2,13 +2,13 @@
 
 class Test {
 public:
-  Test()  { printf("%s,%d: Hello, Project-N!\n", __func__, __LINE__); }
-  ~Test() { printf("%s,%d: Goodbye, Project-N!\n", __func__, __LINE__); }
+    Test()  { printf("%s,%d: Hello, Project-N!\n", __func__, __LINE__); }
+    ~Test() { printf("%s,%d: Goodbye, Project-N!\n", __func__, __LINE__); }
 };
 
 Test test;
 
 int main() {
-  printf("%s,%d: Hello world!\n", __func__, __LINE__);
-  return 0;
+    printf("%s,%d: Hello world!\n", __func__, __LINE__);
+    return 0;
 }
