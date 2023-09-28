e5c35db0b3a93671a0cf9eae7927c33cfe05502a
tracer-ysyx <tracer@ysyx.org>
2023-09-28 16:55:51 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:55:51 up  2:22,  1 user,  load average: 2.59, 2.04, 1.85

--- navy-apps/apps/nslider/src/main.cpp
+++ navy-apps/apps/nslider/src/main.cpp
@@ -27,6 +27,7 @@ void render() {
   sprintf(fname, path, cur);
   slide = SDL_LoadBMP(fname);
   assert(slide);
+  printf("hello\n");
   SDL_UpdateRect(slide, 0, 0, 0, 0);
 }
 
@@ -52,8 +53,7 @@ int main() {
 
   render();
 
-  while(1)
-      ;
+//   while(1)
 
   while (1) {
     SDL_Event e;
