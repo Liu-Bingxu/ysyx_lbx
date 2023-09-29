9e534f5e2d57736b8c11a04fa664429795120cdf
tracer-ysyx <tracer@ysyx.org>
2023-09-29 18:22:50 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:22:50 up 10:10,  1 user,  load average: 2.05, 2.15, 2.12

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
 
     // load program here
 
-    naive_uload(NULL, "/bin/nterm");
+    naive_uload(NULL, "/bin/bird");
 }
 
 Context* schedule(Context *prev) {
--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -4,6 +4,7 @@
 
 #define SDL_STBIMAGE_IMPLEMENTATION
 #include "SDL_stbimage.h"
+#include "stdio.h"
 
 SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
   assert(src->type == RW_TYPE_MEM);
@@ -12,7 +13,25 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+    FILE *img_fp = NULL;
+    img_fp = fopen(filename, "rb");
+    assert(img_fp);
+    fseek(img_fp, 0, SEEK_END);
+    size_t size = ftell(img_fp);
+    fseek(img_fp, 0, SEEK_SET);
+    void *buf = NULL;
+    buf = malloc(size);
+    assert(buf);
+    int len=fread(buf,size,1,img_fp);
+    assert(len == 1);
+    SDL_Surface *face = NULL;
+    face=STBIMG_LoadFromMemory(buf, size);
+    assert(face);
+    fclose(img_fp);
+    img_fp = NULL;
+    free(buf);
+    buf = NULL;
+    return face;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
