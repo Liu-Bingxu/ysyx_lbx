33cd62e9d96519bf166b273284283279bfbf1136
tracer-ysyx <tracer@ysyx.org>
2023-09-29 13:39:20 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:20 up  5:27,  1 user,  load average: 3.08, 2.29, 1.99

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -9,7 +9,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
     // assert(0);
     assert(dst && src);
     assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
-    int x, y, w, h;
+    int x, y;
+    int w, h;
     if (srcrect == NULL){
         w = src->w;
         h = src->h;
@@ -30,8 +31,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
         assert(x <= dst->w);
         assert(y <= dst->h);
     }
-    printf("x is %d, y is %d, w is %d, h is %d\n", x, y, w, h);
-    printf("w1 is %d, h1 is %d, w2 is %d, h2 is %d\n", src->w, src->h, dst->w, dst->h);
+    // printf("x is %d, y is %d, w is %u, h is %u\n", x, y, w, h);
+    // printf("w1 is %d, h1 is %d, w2 is %d, h2 is %d\n", src->w, src->h, dst->w, dst->h);
     assert((x + w) <= dst->w);
     assert((y + h) <= dst->h);
     for (int i = 0; i < h;i++){
