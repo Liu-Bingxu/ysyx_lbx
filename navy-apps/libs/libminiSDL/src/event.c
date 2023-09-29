a9d6db6a788f85a0d17322b2ee57b7e979c6be32
tracer-ysyx <tracer@ysyx.org>
2023-09-29 11:47:10 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:47:10 up  3:35,  1 user,  load average: 3.25, 2.76, 2.63

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -24,25 +24,28 @@ int SDL_PollEvent(SDL_Event *ev) {
 int SDL_WaitEvent(SDL_Event *event) {
     // assert(0);
     char buf[64];
-    while(!NDL_PollEvent(buf,sizeof(buf)));
-    if(buf[1]=='u'){
-        event->type = SDL_KEYUP;
-    }
-    else if(buf[1]=='d'){
-        event->type = SDL_KEYDOWN;
-    }
-    else{
-        printf("%s\n", buf);
-        assert(0);
-    }
-    for (int i = 0; i < (sizeof(keyname) / sizeof(char *));i++){
-        if(strcmp((buf+3),keyname[i])==0){
-            event->key.keysym.sym = i;
-            printf("%s\n", buf);
-            return 1;
+    while(1){
+        if (NDL_PollEvent(buf, sizeof(buf))){
+            if (buf[1] == 'u'){
+                event->type = SDL_KEYUP;
+            }
+            else if (buf[1] == 'd'){
+                event->type = SDL_KEYDOWN;
+            }
+            else{
+                printf("%s\n", buf);
+                assert(0);
+            }
+            for (int i = 0; i < (sizeof(keyname) / sizeof(char *));i++){
+                if(strcmp((buf+3),keyname[i])==0){
+                    event->key.keysym.sym = i;
+                    printf("%s\n", buf);
+                    return 1;
+                }
+            }
+            return 0;
         }
     }
-    return 0;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
