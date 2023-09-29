37d84038482e12f930b48af63b07d00c5aec783a
tracer-ysyx <tracer@ysyx.org>
2023-09-29 11:54:30 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:54:30 up  3:42,  1 user,  load average: 3.05, 2.97, 2.78

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -39,7 +39,8 @@ int SDL_WaitEvent(SDL_Event *event) {
                 printf("%s\n", buf);
                 assert(0);
             }
-            for (int i = 0; i < (sizeof(keyname) / sizeof(char *));i++){
+            printf("%s\n", buf + 3);
+            for (int i = 0; i < (sizeof(keyname) / sizeof(char *)); i++){
                 if(strcmp((buf+3),keyname[i])==0){
                     event->key.keysym.sym = i;
                     printf("%s\n", buf);
