b67445178ac180b9ccd62ab3bbfab72f93d1ecff
tracer-ysyx <tracer@ysyx.org>
2023-10-02 20:16:54 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:16:54 up  6:06,  1 user,  load average: 1.32, 0.95, 0.54

--- navy-apps/libs/libam/src/ioe.c
+++ navy-apps/libs/libam/src/ioe.c
@@ -79,7 +79,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd){
         kbd->keydown = 0;
     }
     else if(buf[1]=='d'){
-        kbd->keydown = 0;
+        kbd->keydown = 1;
     }
     else{
         assert(0);
