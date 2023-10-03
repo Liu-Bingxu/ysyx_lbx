7de7e2a4da1fd073b22cc3a995c9e892839caf9c
tracer-ysyx <tracer@ysyx.org>
2023-10-03 13:44:58 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:44:58 up 13:08,  1 user,  load average: 4.60, 3.12, 2.54

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -128,7 +128,7 @@ typedef	__uint128_t fixedptud;
 
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-    assert(0);
+    // assert(0);
     return (A * B);
 }
 
