09d64a6370b045fc531e02f75b2a133990f4a193
tracer-ysyx <tracer@ysyx.org>
2023-10-03 16:41:34 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:41:34 up 16:05,  1 user,  load average: 1.98, 2.06, 2.18

--- navy-apps/libs/libos/src/crt0/crt0.c
+++ navy-apps/libs/libos/src/crt0/crt0.c
@@ -5,8 +5,9 @@
 int main(int argc, char *argv[], char *envp[]);
 extern char **environ;
 void call_main(uintptr_t *args) {
-  char *empty[] =  {NULL };
-  environ = empty;
-  exit(main(0, empty, empty));
-  assert(0);
+    char *empty[] = {NULL};
+    environ = empty;
+    __libc_init_array();
+    exit(main(0, empty, empty));
+    assert(0);
 }
