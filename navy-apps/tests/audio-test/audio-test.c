1750c598d8796258e0e4d4e51e2fdb049ad0f877
tracer-ysyx <tracer@ysyx.org>
2023-10-03 16:12:40 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:40 up 15:36,  1 user,  load average: 2.62, 2.70, 2.52

--- navy-apps/tests/audio-test/audio-test.c
+++ navy-apps/tests/audio-test/audio-test.c
@@ -10,16 +10,6 @@
 
 int main(int argc , char* argv[]){
 
-    // struct timeval tv,tv_now;
-
-    // int succes = gettimeofday(&tv, NULL);
-
-    // assert(succes == 0);
-
-    // printf("Now sec is %ld, and usec is %ld\n", tv.tv_sec, tv.tv_usec);
-
-    // printf("the size of timeval is %d, the size of tv_sec is %d, the size of tv_usec is %d\n", sizeof(struct timeval), sizeof(tv.tv_sec), sizeof(tv.tv_usec));
-
     NDL_OpenAudio(44100, 2, 4096);
     FILE *fp = fopen(MUSIC_PATH, "r");
     assert(fp);
@@ -39,8 +29,7 @@ int main(int argc , char* argv[]){
         int len = (audio_len - nplay > 4096 ? 4096 : audio_len - nplay);
         NDL_PlayAudio(buf + nplay, len);
         printf("Already play %d/%d bytes of data\n", nplay, audio_len);
-        if (nplay >= audio_len)
-        {
+        if (nplay >= audio_len){
             nplay = 0;
         }
     }
