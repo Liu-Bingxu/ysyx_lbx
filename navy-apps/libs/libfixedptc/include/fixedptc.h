fcd2a785fee4c2c55fd4b608d553e0cb7e35ffff
tracer-ysyx <tracer@ysyx.org>
2023-10-03 13:42:56 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:42:56 up 13:06,  1 user,  load average: 2.58, 2.23, 2.21

--- navy-apps/libs/libfixedptc/include/fixedptc.h
+++ navy-apps/libs/libfixedptc/include/fixedptc.h
@@ -128,35 +128,35 @@ typedef	__uint128_t fixedptud;
 
 /* Multiplies a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_muli(fixedpt A, int B) {
-    // assert(0);
+    assert(0);
     return (A * B);
 }
 
 /* Divides a fixedpt number with an integer, returns the result. */
 static inline fixedpt fixedpt_divi(fixedpt A, int B) {
-    // assert(0);
+    assert(0);
     return (A / B);
 }
 
 /* Multiplies two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_mul(fixedpt A, fixedpt B) {
-    // assert(0);
+    assert(0);
     return (((fixedptd)A * (fixedptd)B) >> 8);
 }
 
 /* Divides two fixedpt numbers, returns the result. */
 static inline fixedpt fixedpt_div(fixedpt A, fixedpt B) {
-    // assert(0);
+    assert(0);
     return (A / (B >> 8));
 }
 
 static inline fixedpt fixedpt_abs(fixedpt A) {
-    // assert(0);
+    assert(0);
     return (A >= 0) ? A : (-A);
 }
 
 static inline fixedpt fixedpt_floor(fixedpt A) {
-    // assert(0);
+    assert(0);
     if(fixedpt_fracpart(A)==0){
         return A;
     }
