9a9355597c4c12234e70b897d1aa6827c940a555
tracer-ysyx <tracer@ysyx.org>
2023-10-07 02:36:36 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:36:36 up 1 day, 16:12,  1 user,  load average: 1.55, 1.37, 1.15

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -109,6 +109,7 @@ void do_syscall(Context *c) {
         break;
     case SYS_write:
         c->GPRx = sys_write(a[1], (void *)a[2], a[3]);
+        printf("Hello\n");
         // Log("syscall the %s with %d %d %d return the %d", sys_call_name[a[0]], a[1], a[2], a[3], c->GPRx);
         break;
     case SYS_lseek:
