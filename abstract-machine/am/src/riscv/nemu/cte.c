4011348bd3e9f5833a81cb00baa85b9e80fe4d0c
tracer-ysyx <tracer@ysyx.org>
2023-10-06 15:45:57 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-33-generic #33~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 10:33:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:45:57 up 1 day,  8:12,  1 user,  load average: 1.59, 1.18, 0.83

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -18,6 +18,7 @@ Context* __am_irq_handle(Context *c) {
             ev.event=EVENT_YIELD;
             if(c->GPR1!=-1)
                 ev.event = EVENT_SYSCALL;
+            c->mepc += 4;
             break;
         default:
             ev.event = EVENT_ERROR;
