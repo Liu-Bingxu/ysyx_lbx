01ef5b9765c45a8ad336cfcf33d85ab7c3b621c1
tracer-ysyx <tracer@ysyx.org>
2023-10-07 21:21:33 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:21:33 up  3:26,  1 user,  load average: 1.80, 2.39, 2.67

--- navy-apps/apps/nterm/src/builtin-sh.cpp
+++ navy-apps/apps/nterm/src/builtin-sh.cpp
@@ -39,28 +39,29 @@ static void sh_handle_cmd(char *cmd) {
         return;
     }
     char *filename = strtok(cmd, " ");
-    char **argv = (char **)malloc(sizeof(char *) * 10);
-    assert(argv);
-    int _cnt = 0;
-    while (1){
-        char *val = strtok(NULL, " ");
-        if (_cnt < (sizeof(argv) / sizeof(char *))){
-            argv[_cnt] = val;
-        }
-        else{
-            char **_argv = (char **)realloc((void *)argv, sizeof(argv) + sizeof(char *) * 1);
-            free(argv);
-            argv = _argv;
-            argv[_cnt] = val;
-        }
-        _cnt++;
-        if(val==NULL){
-            break;
-        }
-    }
-    char *env[] = {NULL};
-    // execve(cmd, argv,env);
-    execl("/bin/pal","/bin/pal","--skip",NULL);
+    char *argv = strtok(NULL, " ");
+    // char **argv = (char **)malloc(sizeof(char *) * 10);
+    // assert(argv);
+    // int _cnt = 0;
+    // while (1){
+    //     char *val = strtok(NULL, " ");
+    //     if (_cnt < (sizeof(argv) / sizeof(char *))){
+    //         argv[_cnt] = val;
+    //     }
+    //     else{
+    //         char **_argv = (char **)realloc((void *)argv, sizeof(argv) + sizeof(char *) * 1);
+    //         free(argv);
+    //         argv = _argv;
+    //         argv[_cnt] = val;
+    //     }
+    //     _cnt++;
+    //     if(val==NULL){
+    //         break;
+    //     }
+    // }
+    // char *env[] = {NULL};
+    execl(cmd,cmd,argv,NULL);
+    // execl("/bin/pal","/bin/pal","--skip",NULL);
 }
 
 void builtin_sh_run() {
