79e720d80dc08636f710c8e93360bb73b8add9be
tracer-ysyx <tracer@ysyx.org>
2023-10-07 21:23:42 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:23:42 up  3:28,  1 user,  load average: 2.61, 2.72, 2.77

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -97,7 +97,7 @@ void init_proc() {
 
 Context* schedule(Context *prev) {
     current->cp = prev;
-    // current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
-    current = &pcb[1];
+    current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+    // current = &pcb[1];
     return current->cp;
 }
