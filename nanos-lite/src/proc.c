5d91ea8220fc5fb6fce2445fdc5c281fb238cb30
tracer-ysyx <tracer@ysyx.org>
2023-10-07 20:22:36 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:22:36 up  2:27,  1 user,  load average: 3.38, 2.39, 1.85

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -83,7 +83,7 @@ void init_proc() {
     char *argv[] = {"0","--skip","Hello World","byebye",NULL};
     char *envp[] = {"PATH=/bin",NULL};
     context_kload(&pcb[0], hello_fun, "It is 1");
-    context_uload(&pcb[1], "/bin/exec-test", argv, envp);
+    context_uload(&pcb[1], "/bin/nwm", argv, envp);
     switch_boot_pcb();
 
     Log("Initializing processes...");
