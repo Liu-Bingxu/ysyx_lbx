70b70f67afc4c05ceb1868a2022cca60c934e573
tracer-ysyx <tracer@ysyx.org>
2023-10-07 20:58:59 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:58:59 up  3:03,  1 user,  load average: 1.90, 2.81, 2.85

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -80,10 +80,10 @@ void context_uload(PCB *pcb, const char *filename, char *const argv[], char *con
 }
 
 void init_proc() {
-    char *argv[] = {"0","--skip","Hello World","byebye",NULL};
+    // char *argv[] = {"0","--skip","Hello World","byebye",NULL};
     char *envp[] = {"PATH=/bin",NULL};
     context_kload(&pcb[0], hello_fun, "It is 1");
-    context_uload(&pcb[1], "/bin/nterm", argv, envp);
+    context_uload(&pcb[1], "/bin/nterm", NULL, envp);
     switch_boot_pcb();
 
     Log("Initializing processes...");
