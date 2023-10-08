63159dd5d9a1a3e004bedcd20052caa324eb2eeb
tracer-ysyx <tracer@ysyx.org>
2023-10-08 22:06:07 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:06:07 up 12:49,  1 user,  load average: 3.15, 3.89, 4.61

--- npc/include/config.h
+++ npc/include/config.h
@@ -2,12 +2,15 @@
 #define __DONFIG_H__
 
 // #define CONFIG_TRACE        1
-// #define CONFIG_ITRACE       1
+#define CONFIG_ITRACE       1
 // #define CONFIG_WATCHPOINT   1
 // #define CONFIG_FTRACE       1
 // #define CONFIG_MTRACE       1
 #define CONFIG_DIFFTEST     1
 // #define CONFIG_VCD_GET      1
 // #define CONFIG_GET_TIMER    1
+
+#define CONFIG_ITRACE_COND
+#define ITRACE_COND 1
 
 #endif
