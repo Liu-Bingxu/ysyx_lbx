0be81f183e1fd93faec4bf28a3d9e1a4c3ca22ef
tracer-ysyx <tracer@ysyx.org>
2023-10-12 23:07:26 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:07:26 up  1:20,  1 user,  load average: 2.08, 1.44, 1.43

--- npc/csrc/regs.c
+++ npc/csrc/regs.c
@@ -129,8 +129,8 @@ void init_ref(CPU_state *cpu){
     {
         cpu->gpr[i] = get_gpr(i);
     }
-    cpu->pc = 0x80000000;
-    cpu->mepc=isa_reg_str2val("mepc", &text);
+    cpu->pc = get_gpr(32);
+    cpu->mepc = isa_reg_str2val("mepc", &text);
     cpu->mtvec=isa_reg_str2val("mtvec", &text);
     cpu->mcause=isa_reg_str2val("mcause", &text);
     cpu->mstatus=isa_reg_str2val("mstatus", &text);
