26f88d12856b079056c87678c572f216c323349e
tracer-ysyx <tracer@ysyx.org>
2023-10-17 01:59:15 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:59:15 up 1 day,  8:31,  1 user,  load average: 1.30, 0.84, 0.58

---
+++ nemu/include/memory/cache.h
@@ -0,0 +1,26 @@
+#ifndef __CACHE_H__
+#define __CACHE_H__
+
+#include "common.h"
+
+#define CACHE_ENABLE
+#define BLOCK_WIDTH 6
+#define BLOCK_SIZE 64
+
+typedef struct cache_line{
+    bool valid;
+    paddr_t tag;
+    union {
+        uint32_t cache_mem[BLOCK_SIZE / 4];
+        uint64_t cache_mem_64[BLOCK_SIZE / 8];
+    };
+} cache_line;
+
+#define ASS_NUM 128
+#define ASS_SIZE 8
+
+void init_cache(void);
+uint32_t cache_read(paddr_t addr, size_t len);
+void cache_write(paddr_t addr, size_t len, uint32_t data);
+
+#endif
---
+++ nemu/src/memory/cache.c
@@ -0,0 +1,62 @@
+#include "memory/cache.h"
+#include "memory/paddr.h"
+
+static cache_line cache[ASS_NUM][ASS_SIZE];
+
+static void mem_read(uintptr_t block_num, uint8_t *buf){
+    memcpy(buf, guest_to_host(CONFIG_MBASE) + (block_num << BLOCK_WIDTH), BLOCK_SIZE);
+}
+
+static void mem_write(uintptr_t block_num, const uint8_t *buf){
+    memcpy(guest_to_host(CONFIG_MBASE) + (block_num << BLOCK_WIDTH), buf, BLOCK_SIZE);
+}
+
+static uint32_t __cache_read(uintptr_t addr){
+    addr &= (~0x3);
+    for (int i = 0; i < ASS_SIZE; i++){
+        if ((cache[(addr / BLOCK_SIZE) % ASS_NUM][i].tag == ((addr / BLOCK_SIZE) / ASS_NUM)) && (cache[(addr / BLOCK_SIZE) % ASS_NUM][i].valid == 1)){
+            return cache[(addr / BLOCK_SIZE) % ASS_NUM][i].cache_mem[(addr % BLOCK_SIZE) / 4];
+        }
+    }
+    int rand_num = rand() % ASS_SIZE;
+    mem_read((addr / BLOCK_SIZE), (void *)(cache[(addr / BLOCK_SIZE) % ASS_NUM][rand_num].cache_mem));
+    cache[(addr / BLOCK_SIZE) % ASS_NUM][rand_num].tag = (addr / BLOCK_SIZE) / ASS_NUM;
+    cache[(addr / BLOCK_SIZE) % ASS_NUM][rand_num].valid = 1;
+    return cache[(addr / BLOCK_SIZE) % ASS_NUM][rand_num].cache_mem[(addr % BLOCK_SIZE) / 4];
+}
+
+static void __cache_write(uintptr_t addr, uint32_t data, uint32_t wmask){
+    addr &= (~0x3);
+    for (int i = 0; i < ASS_SIZE; i++){
+        if ((cache[(addr / BLOCK_SIZE) % ASS_NUM][i].tag == ((addr / BLOCK_SIZE) / ASS_NUM)) && (cache[(addr / BLOCK_SIZE) % ASS_NUM][i].valid == 1)){
+            cache[(addr / BLOCK_SIZE) % ASS_NUM][i].cache_mem[(addr % BLOCK_SIZE) / 4] = (cache[(addr / BLOCK_SIZE) % ASS_NUM][i].cache_mem[(addr % BLOCK_SIZE) / 4] & (~wmask)) | (data & (wmask));
+            mem_write(cache[(addr / BLOCK_SIZE) % ASS_NUM][i].tag * ASS_NUM + ((addr / BLOCK_SIZE) % ASS_NUM), (void *)(cache[(addr / BLOCK_SIZE) % ASS_NUM][i].cache_mem));
+            return;
+        }
+    }
+    cache_line temp_write_cache_line;
+    mem_read((addr / BLOCK_SIZE), (void *)(temp_write_cache_line.cache_mem));
+    temp_write_cache_line.cache_mem[(addr % BLOCK_SIZE) / 4] = (temp_write_cache_line.cache_mem[(addr % BLOCK_SIZE) / 4] & (~wmask)) | (data & (wmask));
+    mem_write((addr / BLOCK_SIZE), (void *)(temp_write_cache_line.cache_mem));
+    return;
+}
+
+#define addr_offset_bit(addr) (((addr) & 0x3) * 8)
+
+const uint32_t len2datamask[] = {0x0, 0xff, 0xffff, 0xffffff, 0xffffffff};
+
+uint32_t cache_read(paddr_t addr, size_t len){
+    return (__cache_read(addr) >> addr_offset_bit(addr)) & len2datamask[len];
+}
+
+void cache_write(paddr_t addr, size_t len, uint32_t data){
+    __cache_write(addr, data << addr_offset_bit(addr), len2datamask[len] << addr_offset_bit(addr));
+}
+
+void init_cache(void){
+    for (int i = 0; i < ASS_NUM; i++){
+        for (int y = 0; y < ASS_SIZE; y++){
+            cache[i][y].valid = 0;
+        }
+    }
+}
