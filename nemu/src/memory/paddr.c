021844e652bc9aa29f95894cabc82cc69f08571a
tracer-ysyx <tracer@ysyx.org>
2023-10-17 02:34:46 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  02:34:46 up 1 day,  9:07,  1 user,  load average: 0.63, 0.46, 0.46

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -27,13 +27,14 @@ static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
-#ifndef CACHE_ENABLE
+#ifdef CACHE_ENABLE
 static word_t pmem_read(paddr_t addr, int len) {
   word_t ret = host_read(guest_to_host(addr), len);
   IFDEF(CONFIG_MTRACE, _Log_mem(addr,"Read  Addr: " FMT_PADDR " Data: " FMT_WORD "\n", addr, ret));
   return ret;
 }
-
+#endif
+#ifndef CACHE_ENABLE
 static void pmem_write(paddr_t addr, int len, word_t data) {
   host_write(guest_to_host(addr), len, data);
   IFDEF(CONFIG_MTRACE, _Log_mem(addr,"Write Addr: " FMT_PADDR " Data: " FMT_WORD "\n", addr, data));
@@ -64,7 +65,7 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))){
-  #ifdef CACHE_ENABLE
+  #ifndef CACHE_ENABLE
       return cache_read(addr,len);
     #else
       return pmem_read(addr, len);
