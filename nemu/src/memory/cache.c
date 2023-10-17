2ecb18bf52204ec26a1bd3a7ddb031fb6cc08f74
tracer-ysyx <tracer@ysyx.org>
2023-10-17 21:40:43 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:43 up 38 min,  1 user,  load average: 0.38, 0.56, 0.56

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -4,8 +4,7 @@
 static cache_line cache[ASS_NUM][ASS_SIZE];
 
 static void mem_read(uintptr_t block_num, uint8_t *buf){
-    (*buf) = 10;
-    printf("finish test\n");
+    printf("the block_num is 0x%016lx\n", block_num);
     memcpy(buf, (void *)(guest_to_host(CONFIG_MBASE)) + (block_num << BLOCK_WIDTH), BLOCK_SIZE);
 }
 
