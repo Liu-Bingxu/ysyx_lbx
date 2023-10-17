2bce51afd150abfe1bc1155bd32f3496b821a973
tracer-ysyx <tracer@ysyx.org>
2023-10-17 22:10:15 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:10:15 up  1:07,  1 user,  load average: 0.96, 0.97, 0.85

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -104,6 +104,7 @@ static void statistic() {
     if (g_timer > 0)Log("simulation frequency = " NUMBERIC_FMT " inst/s", g_nr_guest_inst * 1000000 / g_timer);
     else Log("Finish running in less than 1 us and can not calculate the simulation frequency");
     Log("runing time is %ld, without cache is %ld", get_access_mem_time(), get_cache_access_num() * 100);
+    Log("the hit num is %ld, the access num is %ld", get_cache_hit_num(), get_cache_access_num());
     Log("the cache hit rate is %ld", get_cache_hit_num() / get_cache_access_num());
 }
 
