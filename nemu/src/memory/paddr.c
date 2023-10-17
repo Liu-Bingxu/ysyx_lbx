49da0ce50737a764dea1b60ae7f75f46b12739eb
tracer-ysyx <tracer@ysyx.org>
2023-10-17 22:51:17 +0800

    > compile NEMU ysyx 刘炳旭 Linux kuuga-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:51:17 up  1:48,  1 user,  load average: 1.28, 0.82, 0.58

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -85,8 +85,6 @@ void paddr_write(paddr_t addr, int len, word_t data) {
     #endif
       return; 
     }
-    if(addr==0xa0000000)
-        addr = CONFIG_SERIAL_MMIO;
     IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
     out_of_bound(addr);
 }
