ffa580da774d5e9daba4c3e1c51e117a8b3f8dfd
Liu Bingxu <qwe15889844242@163.com>
2026-01-17 22:11:20 +0800

    change nemu-used program use own riscv-compile-gcc

--- abstract-machine/scripts/riscv64-nemu.mk
+++ abstract-machine/scripts/riscv64-nemu.mk
@@ -1,6 +1,8 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
+CROSS_COMPILE := /opt/riscv/riscv-glibc-rv64imac/bin/riscv64-unknown-linux-gnu-
+COMMON_CFLAGS := -fno-pic -march=rv64imac_zicsr -mcmodel=medany -mstrict-align # overwrite
 
 AM_SRCS += riscv/nemu/start.S \
            riscv/nemu/cte.c \
